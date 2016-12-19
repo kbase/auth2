2703115cedac6177cf5ec681ef2b90a799cbda13
Gavin Price <gaprice@lbl.gov>
2016-12-19 13:17:56 -0800

    Add tests for temporary tokens

--- src/us/kbase/auth2/lib/token/TemporaryToken.java
+++ src/us/kbase/auth2/lib/token/TemporaryToken.java
@@ -6,44 +6,64 @@ import static us.kbase.auth2.lib.Utils.addLong;
 import java.util.Date;
 import java.util.UUID;
 
+/** A temporary token.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class TemporaryToken {
 	
-	//TODO TEST
-	//TODO JAVADOC
-	
 	private final String token;
-	private final Date creationDate = new Date();
-	private final Date expirationDate;
+	private final long creationDate = new Date().getTime();
+	private final long expirationDate;
 	private final UUID id = UUID.randomUUID();
 
-	public TemporaryToken(final String token, final long lifetimeInms) {
+	/** Create a new temporary token.
+	 * @param token the token string.
+	 * @param lifetimeInMS the lifetime of the token in milliseconds.
+	 */
+	public TemporaryToken(final String token, final long lifetimeInMS) {
 		checkStringNoCheckedException(token, "token");
-		if (lifetimeInms < 0) {
+		if (lifetimeInMS < 0) {
 			throw new IllegalArgumentException("lifetime must be >= 0");
 		}
 		this.token = token;
-		this.expirationDate = new Date(
-				addLong(creationDate.getTime(), lifetimeInms));
+		this.expirationDate = new Date(addLong(creationDate, lifetimeInMS)).getTime();
 	}
 
+	/** Get the token string.
+	 * @return the token string.
+	 */
 	public String getToken() {
 		return token;
 	}
 	
+	/** Get the date the token was created.
+	 * @return the creation date.
+	 */
 	public Date getCreationDate() {
-		return creationDate;
+		return new Date(creationDate);
 	}
 
+	/** Get the date the token expires.
+	 * @return the expiration date.
+	 */
 	public Date getExpirationDate() {
-		return expirationDate;
+		return new Date(expirationDate);
 	}
 
+	/** Get the token's ID.
+	 * @return the ID.
+	 */
 	public UUID getId() {
 		return id;
 	}
 
+	/** Get a hashed token based on this token.
+	 * 
+	 * uses the {@link HashedToken#hash(String)} method.
+	 * @return a hashed temporary token.
+	 */
 	public TemporaryHashedToken getHashedToken() {
-		return new TemporaryHashedToken(
-				HashedToken.hash(token), id, creationDate, expirationDate);
+		return new TemporaryHashedToken(HashedToken.hash(token), id, creationDate, expirationDate);
 	}
 }
--- src/us/kbase/test/auth2/lib/token/TokenTest.java
+++ src/us/kbase/test/auth2/lib/token/TokenTest.java
@@ -4,11 +4,16 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.util.Date;
+import java.util.UUID;
+
 import org.junit.Test;
 
 import us.kbase.auth2.lib.exceptions.MissingParameterException;
 import us.kbase.auth2.lib.token.IncomingHashedToken;
 import us.kbase.auth2.lib.token.IncomingToken;
+import us.kbase.auth2.lib.token.TemporaryHashedToken;
+import us.kbase.auth2.lib.token.TemporaryToken;
 import us.kbase.auth2.lib.token.TokenType;
 
 public class TokenTest {
@@ -50,12 +55,56 @@ public class TokenTest {
 		failCreateIncomingToken(null);
 	}
 
-	private void failCreateIncomingToken(String token) {
+	private void failCreateIncomingToken(final String token) {
 		try {
 			new IncomingToken(token);
+			fail("created bad incoming token");
 		} catch (MissingParameterException e) {
 			assertThat("correct exception message", e.getMessage(),
 					is("30000 Missing input parameter: token"));
+		}
+	}
+	
+	@Test
+	public void temporaryToken() throws Exception {
+		final TemporaryToken tt = new TemporaryToken("foobar", 5);
+		final Date now = new Date();
+		final Date nowMinus1 = new Date(now.getTime() - 1000);
+		assertThat("incorrect token string", tt.getToken(), is("foobar"));
+		assertThat("incorrect ID class", tt.getId(), is(UUID.class));
+		assertThat("creation date after now", tt.getCreationDate().before(now), is(true));
+		assertThat("creation date > 1 sec before now", tt.getCreationDate().after(nowMinus1), 
+				is(true));
+		assertThat("incorrect expiration date", tt.getExpirationDate(),
+				is(new Date(tt.getCreationDate().getTime() + 5)));
+		
+		final TemporaryToken tt2 = new TemporaryToken("whee", 1);
+		assertThat("same uuid for different instances", tt.getId().equals(tt2.getId()), is(false));
+		
+		final TemporaryHashedToken ht = tt.getHashedToken();
+		assertThat("incorrect token hash", ht.getTokenHash(),
+				is("w6uP8Tcg6K2QR905Rms8iXTlksL6OD1KOWBxTK7wxPI="));
+		assertThat("incorrect id", ht.getId(), is(tt.getId()));
+		assertThat("incorrect creation date", ht.getCreationDate(), is(tt.getCreationDate()));
+		assertThat("incorrect expiration date", ht.getExpirationDate(),
+				is(tt.getExpirationDate()));
+		
+		failCreateTemporaryToken("foo", -1, "lifetime must be >= 0");
+		failCreateTemporaryToken(null, 0, "Missing argument: token");
+		failCreateTemporaryToken("", 0, "Missing argument: token");
+		failCreateTemporaryToken("\n", 0, "Missing argument: token");
+	}
+	
+	private void failCreateTemporaryToken(
+			final String token,
+			final long timeInMS,
+			final String exception) {
+		try {
+			new TemporaryToken(token, timeInMS);
+			fail("created bad temporary token");
+		} catch (IllegalArgumentException e) {
+			assertThat("correct exception message", e.getMessage(),
+					is(exception));
 		}
 	}
 	
