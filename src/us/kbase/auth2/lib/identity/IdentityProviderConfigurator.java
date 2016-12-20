2dab928cf8113c55bd6a718d0e1022ccd5b599ff
Gavin Price <gaprice@lbl.gov>
2016-12-20 11:06:35 -0800

    Add java doc for the id provider configurator interface.

--- src/us/kbase/auth2/lib/identity/IdentityProviderConfigurator.java
+++ src/us/kbase/auth2/lib/identity/IdentityProviderConfigurator.java
@@ -1,9 +1,20 @@
 package us.kbase.auth2.lib.identity;
 
+/** A configuration agent for an identity provider. Given an identity provider configuration,
+ * properly creates and configures an identity provider.
+ * @author gaprice@lbl.gov
+ *
+ */
 public interface IdentityProviderConfigurator {
 	
-	//TODO JAVADOC
-	
+	/** Given a configuration, creates an identity provider.
+	 * @param cfg the identity provider configuration.
+	 * @return the new identity provider.
+	 */
 	IdentityProvider configure(IdentityProviderConfig cfg);
+	
+	/** Get the name of the identity provider this configurator is able to configure.
+	 * @return the identity provider name.
+	 */
 	String getProviderName();
 }
