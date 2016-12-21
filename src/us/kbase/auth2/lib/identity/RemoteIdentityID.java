9041a5aa7aea89c84a47055896094c9c856cfc10
Gavin Price <gaprice@lbl.gov>
2016-12-21 11:56:45 -0800

    Add tests for remote identity id

--- src/us/kbase/auth2/lib/identity/RemoteIdentityID.java
+++ src/us/kbase/auth2/lib/identity/RemoteIdentityID.java
@@ -1,30 +1,40 @@
 package us.kbase.auth2.lib.identity;
 
+/** An ID for a remote identity, consisting of the identity provider's name and a unique, immutable
+ * ID for the identity created by the provider.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class RemoteIdentityID {
 
-	//TODO TEST
-	//TODO JAVADOC
-	
 	private final String provider;
 	private final String id;
 
+	/** Create a remote identity ID.
+	 * @param provider the name of the provider that is providing the identity.
+	 * @param id the unique, immutable ID the provider uses for the identity.
+	 */
 	public RemoteIdentityID(final String provider, final String id) {
 		if (provider == null || provider.trim().isEmpty()) {
-			throw new IllegalArgumentException(
-					"provider cannot be null or empty");
+			throw new IllegalArgumentException("provider cannot be null or empty");
 		}
 		if (id == null || id.trim().isEmpty()) {
-			throw new IllegalArgumentException(
-					"id cannot be null or empty");
+			throw new IllegalArgumentException("id cannot be null or empty");
 		}
 		this.provider = provider.trim();
 		this.id = id.trim();
 	}
 	
+	/** Get the provider name.
+	 * @return the provider name.
+	 */
 	public String getProvider() {
 		return provider;
 	}
 	
+	/** Get the identity ID.
+	 * @return the identity ID.
+	 */
 	public String getId() {
 		return id;
 	}
@@ -33,8 +43,8 @@ public class RemoteIdentityID {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((provider == null) ? 0 : provider.hashCode());
+		result = prime * result + id.hashCode();
+		result = prime * result + provider.hashCode();
 		return result;
 	}
 	
@@ -50,18 +60,10 @@ public class RemoteIdentityID {
 			return false;
 		}
 		RemoteIdentityID other = (RemoteIdentityID) obj;
-		if (id == null) {
-			if (other.id != null) {
-				return false;
-			}
-		} else if (!id.equals(other.id)) {
+		if (!id.equals(other.id)) {
 			return false;
 		}
-		if (provider == null) {
-			if (other.provider != null) {
-				return false;
-			}
-		} else if (!provider.equals(other.provider)) {
+		if (!provider.equals(other.provider)) {
 			return false;
 		}
 		return true;
--- src/us/kbase/test/auth2/lib/identity/RemoteIdentityTest.java
+++ src/us/kbase/test/auth2/lib/identity/RemoteIdentityTest.java
@@ -7,13 +7,14 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 import us.kbase.auth2.lib.identity.RemoteIdentityDetails;
+import us.kbase.auth2.lib.identity.RemoteIdentityID;
 
 
 public class RemoteIdentityTest {
 	
 	@Test
-	public void detailsWithAllFields() throws Exception {
-		final RemoteIdentityDetails dets = new RemoteIdentityDetails("user", "full", "email");
+	public void remoteDetailsWithAllFields() throws Exception {
+		final RemoteIdentityDetails dets = new RemoteIdentityDetails("user ", " full", "\temail");
 		assertThat("incorrect username", dets.getUsername(), is("user"));
 		assertThat("incorrect fullname", dets.getFullname(), is("full"));
 		assertThat("incorrect email", dets.getEmail(), is("email"));
@@ -23,7 +24,7 @@ public class RemoteIdentityTest {
 	}
 	
 	@Test
-	public void detailsWithEmptyFields() throws Exception {
+	public void remoteDetailsWithEmptyFields() throws Exception {
 		final RemoteIdentityDetails dets = new RemoteIdentityDetails("user", "\t ", " \n");
 		assertThat("incorrect username", dets.getUsername(), is("user"));
 		assertThat("incorrect fullname", dets.getFullname(), is((String) null));
@@ -42,7 +43,7 @@ public class RemoteIdentityTest {
 	}
 	
 	@Test
-	public void detailsFail() throws Exception {
+	public void remoteDetailsFail() throws Exception {
 		failCreateDetails(null);
 		failCreateDetails("              \n       ");
 	}
@@ -50,7 +51,7 @@ public class RemoteIdentityTest {
 	private void failCreateDetails(final String user) {
 		try {
 			new RemoteIdentityDetails(user, "foo", "bar");
-			fail("created bad user");
+			fail("created bad details");
 		} catch (IllegalArgumentException e) {
 			assertThat("incorrect exception msg", e.getMessage(),
 					is("username cannot be null or empty"));
@@ -58,8 +59,8 @@ public class RemoteIdentityTest {
 	}
 	
 	@Test
-	public void detailsEquals() throws Exception {
-		final RemoteIdentityDetails dets = new RemoteIdentityDetails("foo", "bar", "baz");
+	public void remoteDetailsEquals() throws Exception {
+		final RemoteIdentityDetails dets = new RemoteIdentityDetails(" foo", "bar ", "\nbaz");
 		final RemoteIdentityDetails nulldets = new RemoteIdentityDetails("foo", null, null);
 		
 		//identity
@@ -97,7 +98,55 @@ public class RemoteIdentityTest {
 		//unequal email
 		assertThat("incorrect equals", dets.equals(
 				new RemoteIdentityDetails("foo", "bar", "bad")), is(false));
-		
 	}
 
+	@Test
+	public void remoteId() throws Exception {
+		final RemoteIdentityID id = new RemoteIdentityID("foo", "bar");
+		assertThat("incorrect provider name", id.getProvider(), is("foo"));
+		assertThat("incorrect provider id", id.getId(), is("bar"));
+		assertThat("incorrect hashcode", id.hashCode(), is(3118804));
+		assertThat("incorrect toString()", id.toString(),
+				is("RemoteIdentityID [provider=foo, id=bar]"));
+	}
+	
+	@Test
+	public void remoteIdEquals() throws Exception {
+		final RemoteIdentityID id = new RemoteIdentityID("whee", "whoo");
+		
+		//identity
+		assertThat("incorrect equals", id.equals(id), is(true));
+		//equal
+		assertThat("incorrect equals", id.equals(new RemoteIdentityID("whee", "whoo")), is(true));
+		//null obj
+		assertThat("incorrect equals", id.equals(null), is(false));
+		//class
+		assertThat("incorrect equals", id.equals(new Object()), is(false));
+		//unequal provider
+		assertThat("incorrect equals", id.equals(new RemoteIdentityID("wheo", "whoo")), is(false));
+		//unequal id
+		assertThat("incorrect equals", id.equals(new RemoteIdentityID("whee", "whoe")), is(false));
+	}
+	
+	@Test
+	public void remoteIDFail() throws Exception {
+		final String providererr = "provider cannot be null or empty";
+		final String iderr = "id cannot be null or empty";
+		failCreateID(null, "f", providererr);
+		failCreateID(" \t", "f", providererr);
+		failCreateID("p", null, iderr);
+		failCreateID("p", " \n   \t  ", iderr);
+		
+	}
+	
+	private void failCreateID(final String provider, final String id, final String exception) {
+		try {
+			new RemoteIdentityID(provider, id);
+			fail("created bad id");
+		} catch (IllegalArgumentException e) {
+			assertThat("incorrect exception msg", e.getMessage(),
+					is(exception));
+		}
+	}
+	
 }
