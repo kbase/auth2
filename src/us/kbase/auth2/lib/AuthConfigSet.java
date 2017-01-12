6c90312f44654a0e60734c7c6f5aa0e0ea374f86
Gavin Price <gaprice@lbl.gov>
2017-01-11 17:23:46 -0800

    Add javadoc for authconfigset

--- src/us/kbase/auth2/lib/AuthConfigSet.java
+++ src/us/kbase/auth2/lib/AuthConfigSet.java
@@ -1,13 +1,23 @@
 package us.kbase.auth2.lib;
 
+/** A configuration set for the authentication instance. Contains the configuration of the
+ * authentication instance and a user-definable external configuration that is stored as key-value
+ * configuration values in the authentication storage.
+ * @author gaprice@lbl.gov
+ *
+ * @param <T> the external configuration.
+ */
 public class AuthConfigSet<T extends ExternalConfig> {
 
 	//TODO TEST
-	//TODO JAVADOC
 	
 	private AuthConfig cfg;
 	private T extcfg;
 	
+	/** Create a configuration set.
+	 * @param cfg the authentication instance configuration.
+	 * @param extcfg the external configuration.
+	 */
 	public AuthConfigSet(final AuthConfig cfg, final T extcfg) {
 		if (cfg == null) {
 			throw new NullPointerException("cfg");
@@ -19,10 +29,16 @@ public class AuthConfigSet<T extends ExternalConfig> {
 		this.extcfg = extcfg;
 	}
 
+	/** Get the authentication instance configuration.
+	 * @return the authentication instance configuration.
+	 */
 	public AuthConfig getCfg() {
 		return cfg;
 	}
 
+	/** Get the external configuration.
+	 * @return the external configuration.
+	 */
 	public T getExtcfg() {
 		return extcfg;
 	}
