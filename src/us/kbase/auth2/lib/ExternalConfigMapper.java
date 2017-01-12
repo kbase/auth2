a1ae3a61455820bc6c6a9d774de1fed48fab08a6
Gavin Price <gaprice@lbl.gov>
2017-01-12 12:03:55 -0800

    Add javadoc for external config iterfaces.

--- src/us/kbase/auth2/lib/ExternalConfigMapper.java
+++ src/us/kbase/auth2/lib/ExternalConfigMapper.java
@@ -4,11 +4,25 @@ import java.util.Map;
 
 import us.kbase.auth2.lib.exceptions.ExternalConfigMappingException;
 
-// keys defined in the external config may be missing from the incoming map if they're not set in the storage engine
+/** A mapper for mapping a set of key-value pairs into an ExternalConfig class. The Authentication
+ * instance uses this mapper to convert the set of key-value pairs stored in the Authentication
+ * storage system into an ExternalConfig class.
+ * 
+ * Note that any mapper should handle the possibility that some expected key-value pairs may not be
+ * defined in the Authentication storage system.
+ * 
+ * @author gaprice@lbl.gov
+ *
+ * @param <T> the type of the ExternalConfig that is the target of the mapper.
+ */
 public interface ExternalConfigMapper<T extends ExternalConfig> {
 
-	//TODO JAVADOC
-	
+	/** Convert a set of key-value pairs into an ExternalConfig instance. 
+	 * @param config the key-value pairs.
+	 * @return an ExternalConfig instance.
+	 * @throws ExternalConfigMappingException if the key-value pairs could not be mapped into
+	 * the ExternalConfig instance.
+	 */
 	T fromMap(Map<String, String> config) throws ExternalConfigMappingException;
 	
 }
