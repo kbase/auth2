0a1b1b734af7951afe0b5a91e6ec2c3e8d7e4d49
Gavin Price <gaprice@lbl.gov>
2017-02-07 12:49:07 -0800

    Add javadoc for authuser.

---
+++ src/us/kbase/test/auth2/lib/AuthUserTest.java
@@ -0,0 +1,30 @@
+package us.kbase.test.auth2.lib;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import us.kbase.auth2.lib.AuthUser;
+import us.kbase.auth2.lib.DisplayName;
+import us.kbase.auth2.lib.EmailAddress;
+import us.kbase.auth2.lib.Role;
+import us.kbase.auth2.lib.UserDisabledState;
+import us.kbase.auth2.lib.UserName;
+import us.kbase.auth2.lib.identity.RemoteIdentityWithLocalID;
+
+public class AuthUserTest {
+	
+	//TODO NOW authuser
+
+	@Test
+	public void testAuthUserConstruct() {
+		//TODO TEST
+	}
+}
