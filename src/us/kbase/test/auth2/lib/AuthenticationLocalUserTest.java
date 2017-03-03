6cd2b68647dd1d77003033b1384cb385c78ec93d
Gavin Price <gaprice@lbl.gov>
2017-03-03 12:34:28 -0800

    Finish createLocalUser tests

--- src/us/kbase/test/auth2/lib/AuthenticationLocalUserTest.java
+++ src/us/kbase/test/auth2/lib/AuthenticationLocalUserTest.java
@@ -4,12 +4,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.isA;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -34,10 +29,17 @@ import us.kbase.auth2.lib.Password;
 import us.kbase.auth2.lib.Role;
 import us.kbase.auth2.lib.UserDisabledState;
 import us.kbase.auth2.lib.UserName;
+import us.kbase.auth2.lib.exceptions.DisabledUserException;
+import us.kbase.auth2.lib.exceptions.ErrorType;
+import us.kbase.auth2.lib.exceptions.InvalidTokenException;
+import us.kbase.auth2.lib.exceptions.NoSuchTokenException;
+import us.kbase.auth2.lib.exceptions.NoSuchUserException;
+import us.kbase.auth2.lib.exceptions.UnauthorizedException;
 import us.kbase.auth2.lib.storage.AuthStorage;
 import us.kbase.auth2.lib.token.HashedToken;
 import us.kbase.auth2.lib.token.IncomingToken;
 import us.kbase.auth2.lib.token.TokenType;
+import us.kbase.test.auth2.TestCommon;
 import us.kbase.test.auth2.lib.AuthenticationTester.LocalUserAnswerMatcher;
 import us.kbase.test.auth2.lib.AuthenticationTester.TestAuth;
 
@@ -53,7 +55,54 @@ public class AuthenticationLocalUserTest {
 	 */
 	
 	@Test
-	public void create() throws Exception {
+	public void createWithAdminUser() throws Exception {
+		
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("foo"), Collections.emptySet(), set(Role.ADMIN),
+				Collections.emptySet(), new Date(), new Date(), new UserDisabledState());
+		
+		create(admin);
+	}
+	
+	@Test
+	public void createWithCreateAdminUser() throws Exception {
+		
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("foo"), Collections.emptySet(), set(Role.CREATE_ADMIN),
+				Collections.emptySet(), new Date(), new Date(), new UserDisabledState());
+		
+		create(admin);
+	}
+	
+	@Test
+	public void createWithRootUser() throws Exception {
+		
+		final AuthUser admin = new AuthUser(UserName.ROOT, new EmailAddress("f@g.com"),
+				new DisplayName("foo"), Collections.emptySet(), set(Role.ROOT),
+				Collections.emptySet(), new Date(), new Date(), new UserDisabledState());
+		
+		create(admin);
+	}
+	
+	@Test
+	public void createFailWithoutAdminUser() throws Exception {
+		
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("foo"), Collections.emptySet(), set(Role.SERV_TOKEN),
+				Collections.emptySet(), new Date(), new Date(), new UserDisabledState());
+		createFail(admin, new UnauthorizedException(ErrorType.UNAUTHORIZED));
+	}
+	
+	private void createFail(final AuthUser admin, final Exception expected) {
+		try {
+			create(admin);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, expected);
+		}
+	}
+
+	private void create(final AuthUser adminUser) throws Exception {
 		final TestAuth testauth = initTestAuth();
 		final AuthStorage storage = testauth.storageMock;
 		final Authentication auth = testauth.auth;
@@ -69,10 +118,7 @@ public class AuthenticationLocalUserTest {
 				.thenReturn(new HashedToken(TokenType.LOGIN, null, UUID.randomUUID(), "foobarhash",
 						new UserName("admin"), new Date(), new Date()));
 		
-		when(storage.getUser(new UserName("admin")))
-				.thenReturn(new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
-						new DisplayName("foo"), Collections.emptySet(), set(Role.ADMIN),
-						Collections.emptySet(), new Date(), new Date(), new UserDisabledState()));
+		when(storage.getUser(new UserName("admin"))).thenReturn(adminUser);
 		
 		when(rand.getTemporaryPassword(10)).thenReturn(pwdChar);
 		
@@ -97,7 +143,111 @@ public class AuthenticationLocalUserTest {
 		 * ran
 		 */
 		verify(storage).createLocalUser(any());
+	}
+	
+	@Test
+	public void createUserFailDisabledUser() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
 		
+		final IncomingToken token = new IncomingToken("foobar");
+		
+		when(storage.getToken(token.getHashedToken()))
+				.thenReturn(new HashedToken(TokenType.LOGIN, null, UUID.randomUUID(), "foobarhash",
+						new UserName("admin"), new Date(), new Date()));
+		
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("foo"), Collections.emptySet(), set(Role.SERV_TOKEN),
+				Collections.emptySet(), new Date(), new Date(),
+				new UserDisabledState("disabled", new UserName("foo"), new Date()));
+		
+		when(storage.getUser(new UserName("admin"))).thenReturn(admin);
+		
+		failCreateLocalUser(auth, token, new UserName("foo"), new DisplayName("bar"),
+				new EmailAddress("f@g.com"), new DisabledUserException());
+		
+		verify(storage).deleteTokens(new UserName("admin"));
+	}
+	
+	@Test
+	public void createUserFailInvalidToken() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
+		
+		final IncomingToken token = new IncomingToken("foobar");
+		
+		when(storage.getToken(token.getHashedToken())).thenThrow(new NoSuchTokenException("foo"));
+		
+		failCreateLocalUser(auth, token, new UserName("foo"), new DisplayName("bar"),
+				new EmailAddress("f@g.com"), new InvalidTokenException());
+	}
+	
+	@Test
+	public void createUserFailNoSuchUser() throws Exception {
+		// should never actually happen if db isn't corrupted
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
+		
+		final IncomingToken token = new IncomingToken("foobar");
+		
+		when(storage.getToken(token.getHashedToken()))
+				.thenReturn(new HashedToken(TokenType.LOGIN, null, UUID.randomUUID(), "foobarhash",
+						new UserName("admin"), new Date(), new Date()));
+		
+		when(storage.getUser(new UserName("admin"))).thenThrow(new NoSuchUserException("whee"));
+		
+		failCreateLocalUser(auth, token, new UserName("foo"), new DisplayName("bar"),
+				new EmailAddress("f@g.com"), new RuntimeException("There seems to be an error " +
+						"in the storage system. Token was valid, but no user"));
+	}
+	
+	@Test
+	public void createUserFailNulls() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final Authentication auth = testauth.auth;
+		
+		failCreateLocalUser(auth, null, new UserName("foo"), new DisplayName("bar"),
+				new EmailAddress("f@g.com"), new NullPointerException("token"));
+		
+		failCreateLocalUser(auth, new IncomingToken("whee"), null,
+				new DisplayName("bar"), new EmailAddress("f@g.com"),
+				new NullPointerException("userName"));
+		
+		failCreateLocalUser(auth, new IncomingToken("whee"), new UserName("foo"),
+				null, new EmailAddress("f@g.com"),
+				new NullPointerException("displayName"));
+		
+		failCreateLocalUser(auth, new IncomingToken("whee"), new UserName("foo"),
+				new DisplayName("bar"), null,
+				new NullPointerException("email"));
+	}
+	
+	@Test
+	public void createRootUserFail() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final Authentication auth = testauth.auth;
+		
+		failCreateLocalUser(auth, null, UserName.ROOT, new DisplayName("bar"),
+				new EmailAddress("f@g.com"), new UnauthorizedException(ErrorType.UNAUTHORIZED,
+						"Cannot create ROOT user"));
+	}
+	
+	public void failCreateLocalUser(
+			final Authentication auth,
+			final IncomingToken token,
+			final UserName userName,
+			final DisplayName display,
+			final EmailAddress email,
+			final Exception e) {
+		try {
+			auth.createLocalUser(token, userName, display, email);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, e);
+		}
 	}
 	
 }
