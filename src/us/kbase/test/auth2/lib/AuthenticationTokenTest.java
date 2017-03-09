917201dc31acfdf088e4cc822b8351cf5cd6fe34
Gavin Price <gaprice@lbl.gov>
2017-03-09 15:40:05 -0800

    add getTokens(IncomingToken, UserName) tests

--- src/us/kbase/test/auth2/lib/AuthenticationTokenTest.java
+++ src/us/kbase/test/auth2/lib/AuthenticationTokenTest.java
@@ -17,17 +17,31 @@ import static us.kbase.test.auth2.lib.AuthenticationTester.initTestAuth;
 
 import java.time.Clock;
 import java.time.Instant;
+import java.util.Collections;
+import java.util.Set;
 import java.util.UUID;
 
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import us.kbase.auth2.cryptutils.RandomDataGenerator;
+import us.kbase.auth2.lib.AuthUser;
 import us.kbase.auth2.lib.Authentication;
+import us.kbase.auth2.lib.DisplayName;
+import us.kbase.auth2.lib.EmailAddress;
+import us.kbase.auth2.lib.Role;
+import us.kbase.auth2.lib.UserDisabledState;
 import us.kbase.auth2.lib.UserName;
+import us.kbase.auth2.lib.exceptions.DisabledUserException;
+import us.kbase.auth2.lib.exceptions.ErrorType;
+import us.kbase.auth2.lib.exceptions.IllegalParameterException;
 import us.kbase.auth2.lib.exceptions.InvalidTokenException;
+import us.kbase.auth2.lib.exceptions.MissingParameterException;
 import us.kbase.auth2.lib.exceptions.NoSuchTokenException;
+import us.kbase.auth2.lib.exceptions.NoSuchUserException;
+import us.kbase.auth2.lib.exceptions.UnauthorizedException;
 import us.kbase.auth2.lib.storage.AuthStorage;
+import us.kbase.auth2.lib.storage.exceptions.AuthStorageException;
 import us.kbase.auth2.lib.token.HashedToken;
 import us.kbase.auth2.lib.token.IncomingToken;
 import us.kbase.auth2.lib.token.TokenSet;
@@ -155,6 +169,138 @@ public class AuthenticationTokenTest {
 			final Exception e) {
 		try {
 			auth.getToken(t);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, e);
+		}
+	}
+	
+	@Test
+	public void getTokensUser() throws Exception {
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("bar"), Collections.emptySet(), set(Role.ADMIN),
+				Collections.emptySet(), Instant.now(), null, new UserDisabledState());
+		getTokensUser(admin);
+	}
+	
+	@Test
+	public void getTokensUserFailNonAdmin() throws Exception {
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("bar"), Collections.emptySet(), set(Role.DEV_TOKEN),
+				Collections.emptySet(), Instant.now(), null, new UserDisabledState());
+		failGetTokensUser(admin, new UnauthorizedException(ErrorType.UNAUTHORIZED));
+	}
+	
+	@Test
+	public void getTokensUserFailCreate() throws Exception {
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("bar"), Collections.emptySet(), set(Role.CREATE_ADMIN),
+				Collections.emptySet(), Instant.now(), null, new UserDisabledState());
+		failGetTokensUser(admin, new UnauthorizedException(ErrorType.UNAUTHORIZED));
+	}
+	
+	@Test
+	public void getTokensUserFailRoot() throws Exception {
+		final AuthUser admin = new AuthUser(UserName.ROOT, new EmailAddress("f@g.com"),
+				new DisplayName("bar"), Collections.emptySet(), set(Role.ROOT),
+				Collections.emptySet(), Instant.now(), null, new UserDisabledState());
+		failGetTokensUser(admin, new UnauthorizedException(ErrorType.UNAUTHORIZED));
+	}
+	
+	@Test
+	public void getTokensUserFailDisabled() throws Exception {
+		final AuthUser admin = new AuthUser(new UserName("admin"), new EmailAddress("f@g.com"),
+				new DisplayName("bar"), Collections.emptySet(), set(Role.DEV_TOKEN),
+				Collections.emptySet(), Instant.now(), null,
+				new UserDisabledState("foo", new UserName("baz"), Instant.now()));
+		failGetTokensUser(admin, new DisabledUserException());
+	}
+	
+	@Test
+	public void getTokensUserFailNulls() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final Authentication auth = testauth.auth;
+		
+		failGetTokensUser(auth, null, new UserName("foo"), new NullPointerException("token"));
+		failGetTokensUser(auth, new IncomingToken("foo"), null,
+				new NullPointerException("userName"));
+	}
+	
+	@Test
+	public void getTokensUserFailInvalidToken() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
+		
+		final IncomingToken t = new IncomingToken("foobarbaz");
+		
+		when(storage.getToken(t.getHashedToken())).thenThrow(new NoSuchTokenException("foo"));
+		
+		failGetTokensUser(auth, t, new UserName("bar"), new InvalidTokenException());
+	}
+	
+	@Test
+	public void forceResetAllPasswordsFailCatastrophicNoUser() throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
+		
+		final IncomingToken t = new IncomingToken("foobarbaz");
+		
+		final HashedToken token = new HashedToken(TokenType.LOGIN, null, UUID.randomUUID(),
+				"wubba", new UserName("admin"), Instant.now(), Instant.now());
+		
+		when(storage.getToken(t.getHashedToken())).thenReturn(token, (HashedToken) null);
+		
+		when(storage.getUser(new UserName("admin"))).thenThrow(new NoSuchUserException("admin"));
+		
+		failGetTokensUser(auth, t, new UserName("bar"), new RuntimeException(
+				"There seems to be an error in the storage system. Token was valid, but no user"));
+	}
+
+	private void getTokensUser(final AuthUser admin) throws Exception {
+		final TestAuth testauth = initTestAuth();
+		final AuthStorage storage = testauth.storageMock;
+		final Authentication auth = testauth.auth;
+		
+		final IncomingToken t = new IncomingToken("foobarbaz");
+		
+		final HashedToken token = new HashedToken(TokenType.LOGIN, null, UUID.randomUUID(),
+				"wubba", admin.getUserName(), Instant.now(), Instant.now());
+		
+		when(storage.getToken(t.getHashedToken())).thenReturn(token, (HashedToken) null);
+		
+		when(storage.getUser(admin.getUserName())).thenReturn(admin, (AuthUser) null);
+		
+		when(storage.getTokens(new UserName("foo"))).thenReturn(set(TOKEN1, TOKEN2));
+		
+		try {
+			final Set<HashedToken> tokens = auth.getTokens(t, new UserName("foo"));
+			assertThat("incorrect tokens", tokens, is(set(TOKEN1, TOKEN2)));
+		} catch (Throwable th) {
+			if (admin.isDisabled()) {
+				verify(storage).deleteTokens(admin.getUserName());
+			}
+			throw th;
+		}
+	}
+	
+	private void failGetTokensUser(final AuthUser admin, final Exception e) {
+		try {
+			getTokensUser(admin);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, e);
+		}
+	}
+	
+	private void failGetTokensUser(
+			final Authentication auth,
+			final IncomingToken token,
+			final UserName name,
+			final Exception e) {
+		try {
+			auth.getTokens(token, name);
 			fail("expected exception");
 		} catch (Exception got) {
 			TestCommon.assertExceptionCorrect(got, e);
