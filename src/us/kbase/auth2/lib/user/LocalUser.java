d2e75640b4d66a97b6eb757511037c797f843ea0
Gavin Price <gaprice@lbl.gov>
2017-03-16 21:58:38 -0700

    Update LocalUser javadoc

--- src/us/kbase/auth2/lib/user/LocalUser.java
+++ src/us/kbase/auth2/lib/user/LocalUser.java
@@ -135,9 +135,20 @@ public class LocalUser extends AuthUser {
 		return true;
 	}
 	
+	/** Get a builder for a local user.
+	 * 
+	 * Note that the password hash and salt arrays are not copied when building, so any changes
+	 * to those arrays will be reflected in the user class. This can be useful for clearing the
+	 * array data when no longer needed.
+	 * 
+	 * @param userName the users's user name.
+	 * @param displayName the user's display name.
+	 * @param creationDate the user's creation date.
+	 * @param passwordHash the hash of the user's password.
+	 * @param salt the salt used to hash the password.
+	 * @return a builder.
+	 */
 	public static Builder getBuilder(
-
-			//TODO NOW JAVADOC
 			final UserName userName,
 			final DisplayName displayName,
 			final Instant creationDate,
@@ -147,15 +158,17 @@ public class LocalUser extends AuthUser {
 		
 	}
 	
+	/** A LocalUser builder.
+	 * @author gaprice@lbl.gov
+	 *
+	 */
 	public static class Builder extends AbstractBuilder<Builder> {
-		//TODO NOW JAVADOC
 		
 		private final byte[] passwordHash;
 		private final byte[] salt;
 		private boolean forceReset = false;
 		private Optional<Instant> lastReset = Optional.absent();
 
-		//TODO NOW note passwordhash & salt aren't copied
 		private Builder(
 				final UserName userName,
 				final DisplayName displayName,
@@ -179,20 +192,32 @@ public class LocalUser extends AuthUser {
 			return this;
 		}
 		
+		/** Mark that that user is required to reset their password on the next login.
+		 * @param forceReset true for the user to reset their password.
+		 * @return this builder.
+		 */
 		public Builder withForceReset(final boolean forceReset) {
 			this.forceReset = forceReset;
 			return this;
 		}
 		
+		/** Set the time of the user's last password reset.
+		 * @param lastReset the last time the user reset their password.
+		 * @return this builder.
+		 */
 		public Builder withLastReset(final Instant lastReset) {
 			nonNull(lastReset, "lastReset");
 			this.lastReset = Optional.of(lastReset);
 			return this;
 		}
 		
+		/** Build the user.
+		 * @return the user.
+		 */
 		public LocalUser build() {
 			return new LocalUser(userName, displayName, created, email, roles, customRoles,
-					policyIDs, lastLogin, disabledState, passwordHash, salt, forceReset, lastReset);
+					policyIDs, lastLogin, disabledState, passwordHash, salt, forceReset,
+					lastReset);
 		}
 		
 	}
