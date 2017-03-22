7dc5d6a96a785abd903a4cb5bb811276c9d191a0
Gavin Price <gaprice@lbl.gov>
2017-03-21 20:02:34 -0700

    Make UserUpdate immutable and add a builder
    and hashcode and equals

--- src/us/kbase/auth2/lib/UserUpdate.java
+++ src/us/kbase/auth2/lib/UserUpdate.java
@@ -10,30 +10,14 @@ import com.google.common.base.Optional;
  */
 public class UserUpdate {
 	
-	private Optional<DisplayName> displayName = Optional.absent();
-	private Optional<EmailAddress> email = Optional.absent();
+	private final Optional<DisplayName> displayName;
+	private final Optional<EmailAddress> email;
 	
-	/** Create an update. */
-	public UserUpdate() {}
-	
-	/** Set the display name to be updated.
-	 * @param displayName the name to be updated.
-	 * @return this UserUpdate.
-	 */
-	public UserUpdate withDisplayName(final DisplayName displayName) {
-		nonNull(displayName, "displayName");
-		this.displayName = Optional.of(displayName);
-		return this;
-	}
-	
-	/** Set the email address to be updated.
-	 * @param email the email address to be updated.
-	 * @return this UserUpdate.
-	 */
-	public UserUpdate withEmail(final EmailAddress email) {
-		nonNull(email, "email");
-		this.email = Optional.of(email);
-		return this;
+	private UserUpdate(
+			final Optional<DisplayName> displayName,
+			final Optional<EmailAddress> email) {
+		this.displayName = displayName;
+		this.email = email;
 	}
 	
 	/** Returns the display name for the update, or absent if no update is to be performed.
@@ -42,7 +26,7 @@ public class UserUpdate {
 	public Optional<DisplayName> getDisplayName() {
 		return displayName;
 	}
-
+	
 	/** Returns the email address for the update, or absent if no update is to be performed.
 	 * @return the email address.
 	 */
@@ -55,5 +39,89 @@ public class UserUpdate {
 	 */
 	public boolean hasUpdates() {
 		return displayName.isPresent() || email.isPresent();
+	}
+	
+	/** Get a UserUpdate builder.
+	 * @return a builder.
+	 */
+	public static Builder getBuilder() {
+		return new Builder();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((displayName == null) ? 0 : displayName.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		UserUpdate other = (UserUpdate) obj;
+		if (displayName == null) {
+			if (other.displayName != null) {
+				return false;
+			}
+		} else if (!displayName.equals(other.displayName)) {
+			return false;
+		}
+		if (email == null) {
+			if (other.email != null) {
+				return false;
+			}
+		} else if (!email.equals(other.email)) {
+			return false;
+		}
+		return true;
+	}
+
+	/** A builder for a UserUpdate.
+	 * @author gaprice@lbl.gov
+	 *
+	 */
+	public static class Builder {
+	
+		private Optional<DisplayName> displayName = Optional.absent();
+		private Optional<EmailAddress> email = Optional.absent();
+		
+		private Builder() {}
+
+		/** Set the display name to be updated.
+		 * @param displayName the name to be updated.
+		 * @return this Builder.
+		 */
+		public Builder withDisplayName(final DisplayName displayName) {
+			nonNull(displayName, "displayName");
+			this.displayName = Optional.of(displayName);
+			return this;
+		}
+		
+		/** Set the email address to be updated.
+		 * @param email the email address to be updated.
+		 * @return this Builder.
+		 */
+		public Builder withEmail(final EmailAddress email) {
+			nonNull(email, "email");
+			this.email = Optional.of(email);
+			return this;
+		}
+		
+		/** Build the user update.
+		 * @return a new UserUpdate.
+		 */
+		public UserUpdate build() {
+			return new UserUpdate(displayName, email);
+		}
 	}
 }
