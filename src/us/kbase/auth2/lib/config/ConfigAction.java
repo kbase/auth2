4a2b584384cceba35a377955bb31e8ee52006948
Gavin Price <gaprice@lbl.gov>
2017-03-31 09:55:36 -0700

    Refactor config action to hide action subclasses
    not need to expose them

--- src/us/kbase/auth2/lib/config/ConfigAction.java
+++ src/us/kbase/auth2/lib/config/ConfigAction.java
@@ -4,7 +4,9 @@ package us.kbase.auth2.lib.config;
  * @author gaprice@lbl.gov
  *
  */
-public interface ConfigAction {
+public abstract class ConfigAction {
+	
+	private ConfigAction() {}
 	
 	//TODO NOW TEST
 
@@ -13,38 +15,38 @@ public interface ConfigAction {
 	 * No action is required, but a value for the configuration item may be expected.
 	 * @return true if the action is actually a state.
 	 */
-	public boolean isState();
+	public abstract boolean isState();
 	
 	/** Returns true if no action is required.
 	 * @return true if no action is required.
 	 */
-	public boolean isNoAction();
+	public abstract boolean isNoAction();
 	
 	/** Returns true if this ConfigAction represents a set action - the configuration item should
 	 * be set to a new value.
 	 * @return true if this action is a set.
 	 */
-	public boolean isSet();
+	public abstract boolean isSet();
 	
 	/** Returns true if this ConfigAction represents a remove action - the configuration item
 	 * should be removed from the configuration.
 	 * @return true if this action is a remove.
 	 */
-	public boolean isRemove();
+	public abstract boolean isRemove();
 	
 	static ConfigState state() {
 		return ConfigState.INSTANCE;
 	}
 	
-	static RemoveAction remove() {
+	static Action remove() {
 		return RemoveAction.INSTANCE;
 	}
 	
-	static NoAction noAction() {
+	static Action noAction() {
 		return NoAction.INSTANCE;
 	}
 	
-	static SetAction set() {
+	static Action set() {
 		return SetAction.INSTANCE;
 	}
 	
@@ -52,7 +54,7 @@ public interface ConfigAction {
 	 * @author gaprice@lbl.gov
 	 *
 	 */
-	public final static class ConfigState implements ConfigAction {
+	public static final class ConfigState extends ConfigAction {
 		
 		private static final ConfigState INSTANCE = new ConfigState();
 		
@@ -83,14 +85,12 @@ public interface ConfigAction {
 	 * @author gaprice@lbl.gov
 	 *
 	 */
-	public interface Action extends ConfigAction {}
+	public static abstract class Action extends ConfigAction {
+		
+		private Action() {}
+	}
 	
-	
-	/** A remove action.
-	 * @author gaprice@lbl.gov
-	 *
-	 */
-	public final class RemoveAction implements Action {
+	private static final class RemoveAction extends Action {
 		
 		private static final RemoveAction INSTANCE = new RemoveAction();
 		
@@ -117,11 +117,7 @@ public interface ConfigAction {
 		}
 	}
 	
-	/** A set action.
-	 * @author gaprice@lbl.gov
-	 *
-	 */
-	public final class SetAction implements Action {
+	private static final class SetAction extends Action {
 		
 		private static final SetAction INSTANCE = new SetAction();
 		
@@ -148,11 +144,7 @@ public interface ConfigAction {
 		}
 	}
 	
-	/** An action that represents that no action is required.
-	 * @author gaprice@lbl.gov
-	 *
-	 */
-	public final class NoAction implements Action {
+	private static final class NoAction extends Action {
 		
 		private static final NoAction INSTANCE = new NoAction();
 
