e1ccdb17b7b595629bed0b0d22f29c1584cbd180
Gavin Price <gaprice@lbl.gov>
2018-01-01 16:34:26 -0800

    Add test mode token creation endpoint.
    Allows creating tokens without authentication for any user.

--- src/us/kbase/auth2/service/api/NewAPIToken.java
+++ src/us/kbase/auth2/service/api/NewAPIToken.java
@@ -16,5 +16,31 @@ public class NewAPIToken extends APIToken {
 	public String getToken() {
 		return token;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		NewAPIToken other = (NewAPIToken) obj;
+		if (token == null) {
+			if (other.token != null) {
+				return false;
+			}
+		} else if (!token.equals(other.token)) {
+			return false;
+		}
+		return true;
+	}
 }
