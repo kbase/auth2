49dc35dfe366ee447517d60d5f5750a4512c302c
Gavin Price <gaprice@lbl.gov>
2018-01-18 12:10:54 -0800

    Make the test controller smarter about start up
    In some environments 10s isn't enough time for the server to start up.

--- src/us/kbase/test/auth2/authcontroller/AuthController.java
+++ src/us/kbase/test/auth2/authcontroller/AuthController.java
@@ -8,16 +8,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 
 import us.kbase.common.test.TestException;
@@ -31,13 +36,14 @@ public class AuthController {
 	private final Process auth;
 	private final int port;
 	private final Path tempDir;
+	private final String version;
 	
 	public AuthController(
 			final Path jarsDir,
 			final String mongoHost,
 			final String mongoDatabase,
 			final Path rootTempDir)
-			throws IOException, InterruptedException {
+			throws Exception {
 		final String classPath = getClassPath(jarsDir);
 		tempDir = makeTempDirs(rootTempDir, "AuthController-", Collections.emptyList());
 		port = findFreePort();
@@ -54,8 +60,41 @@ public class AuthController {
 				.redirectOutput(tempDir.resolve("auth.log").toFile());
 		
 		auth = servpb.start();
-		// could make a loop checking for connection later
-		Thread.sleep(10000); //wait for server to start up
+		
+		Exception startupError = null;
+		String version = null;
+		for (int i = 0; i < 40; i++) {
+			Thread.sleep(1000); //wait for server to start up
+			final HttpURLConnection authConn =
+					(HttpURLConnection) new URL("http://localhost:" + port).openConnection();
+			try {
+				authConn.setRequestProperty("accept", "application/json");
+				authConn.connect();
+				if (authConn.getResponseCode() != 200) {
+					try (final InputStream is = authConn.getErrorStream()) {
+						startupError = new TestException(IOUtils.toString(is));
+					}
+				} else {
+					try (final InputStream is = authConn.getInputStream()) {
+						
+						@SuppressWarnings("unchecked")
+						final Map<String, Object> resp =
+								new ObjectMapper().readValue(is, Map.class);
+						version = (String) resp.get("version");
+						startupError = null;
+						break;
+					}
+				}
+			} catch (Exception e) {
+				startupError = e;
+			}
+		}
+		if (startupError != null) {
+			throw startupError;
+		} else {
+			this.version = version;
+		}
+		
 	}
 	
 	public int getServerPort() {
@@ -64,6 +103,10 @@ public class AuthController {
 
 	public Path getTempDir() {
 		return tempDir;
+	}
+	
+	public String getVersion() {
+		return version;
 	}
 
 	public void destroy(boolean deleteTempFiles) throws IOException {
@@ -98,7 +141,7 @@ public class AuthController {
 		return String.join(":", classpath);
 	}
 	
-	public static void main(final String[] args) throws IOException, InterruptedException {
+	public static void main(final String[] args) throws Exception {
 		final AuthController ac = new AuthController(
 				Paths.get("/home/crusherofheads/localgit/jars/lib/jars"),
 				"localhost:27017",
@@ -106,6 +149,7 @@ public class AuthController {
 				Paths.get("authtesttemp"));
 		System.out.println(ac.getServerPort());
 		System.out.println(ac.getTempDir());
+		System.out.println(ac.getVersion());
 		Scanner reader = new Scanner(System.in);
 		System.out.println("any char to shut down");
 		//get user input for a
