a434f0123959502eb76e9f44cbe9a88ecc4c7ea8
Gavin Price <gaprice@lbl.gov>
2018-03-26 10:43:08 -0700

    Bump version

--- src/us/kbase/auth2/service/ui/Root.java
+++ src/us/kbase/auth2/service/ui/Root.java
@@ -24,7 +24,7 @@ public class Root {
 	
 	//TODO JAVADOC or swagger
 	
-	private static final String VERSION = "0.2.3";
+	private static final String VERSION = "0.2.4";
 	
 	@GET
 	@Template(name = "/root")
