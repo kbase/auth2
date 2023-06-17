51e95758aa624b1449d1721817574d91006a7c10
Gavin <gaprice@lbl.gov>
2023-06-16 18:03:56 -0700

    Move recanonicalization test helper methods to common file
    Will be needed for the auth CLI tests

--- src/us/kbase/test/auth2/lib/storage/mongo/MongoStorageRecanonicalizationTest.java
+++ src/us/kbase/test/auth2/lib/storage/mongo/MongoStorageRecanonicalizationTest.java
@@ -3,108 +3,50 @@ package us.kbase.test.auth2.lib.storage.mongo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
-import static us.kbase.test.auth2.TestCommon.set;
+import static us.kbase.test.auth2.lib.storage.mongo.MongoStorageTestCommon.getRecanonicalizationFlag;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.bson.Document;
 import org.junit.Test;
 
 import com.github.zafarkhaja.semver.Version;
 
-import us.kbase.auth2.lib.DisplayName;
-import us.kbase.auth2.lib.UserName;
-import us.kbase.auth2.lib.identity.RemoteIdentity;
-import us.kbase.auth2.lib.identity.RemoteIdentityDetails;
-import us.kbase.auth2.lib.identity.RemoteIdentityID;
 import us.kbase.auth2.lib.storage.exceptions.AuthStorageException;
-import us.kbase.auth2.lib.user.NewUser;
 import us.kbase.test.auth2.TestCommon;
 
 public class MongoStorageRecanonicalizationTest extends MongoStorageTester {
 	
-	private static final String RECANONICALIZATION_FLAG = "_recanonicalized_for_version_";
-	
-	private static final Instant NOW = Instant.now()
-			.truncatedTo(ChronoUnit.MILLIS); // mongo truncates
-	
-	private RemoteIdentity createRemoteIdentity(final int id) {
-		return new RemoteIdentity(
-				new RemoteIdentityID("prov", "bar" + id),
-				new RemoteIdentityDetails("user", "full", "email")
-		);
-	}
-	
 	private void createUser(final String username, final String displayName, final int remoteID)
 			throws Exception {
-		storage.createUser(NewUser.getBuilder(
-				new UserName(username),
-				new DisplayName(displayName),
-				NOW,
-				createRemoteIdentity(remoteID)
-		).build());
-	}
-	
-	private String getFlag(final String version) {
-		return RECANONICALIZATION_FLAG + version;
+		MongoStorageTestCommon.createUser(storage, username, displayName, remoteID);
 	}
 	
 	private void trashCanonicalizationData(final String username) {
-		db.getCollection("users").updateOne(
-				new Document("user", username),
-				new Document("$set",
-						new Document("dispcan", Arrays.asList("messed", "up", username)))
-		);
+		MongoStorageTestCommon.trashCanonicalizationData(db, username);
 	}
 	
 	private void setRecanonicalizedFlag(final String username, final String version) {
-		db.getCollection("users").updateOne(
-				new Document("user", username),
-				new Document("$set", new Document(getFlag(version), true))
-		);
+		MongoStorageTestCommon.setRecanonicalizedFlag(db, username, version);
 	}
 	
 	private void assertNoRecanonicalizationFlag(final String username) {
-		final Document user = getUserDoc(username);
-		for (final String key: user.keySet()) {
-			if (key.startsWith(RECANONICALIZATION_FLAG)) {
-				fail(String.format("Found recanonicalization flag %s for user %s", key, username));
-			}
-		}
+		MongoStorageTestCommon.assertNoRecanonicalizationFlag(db, username);
 	}
-	
 	
 	private void assertOneRecanonicalizationFlag(final String username, final String version) {
-		final Document user = getUserDoc(username);
-		final Set<String> flags = user.keySet().stream()
-				.filter(k -> k.startsWith(RECANONICALIZATION_FLAG)).collect(Collectors.toSet());
-		assertThat("incorrect recanonicalization flags", flags, is(set(getFlag(version))));
-	}
-	
-	private Document getUserDoc(final String username) {
-		return db.getCollection("users").find(new Document("user", username)).first();
+		MongoStorageTestCommon.assertOneRecanonicalizationFlag(db, username, version);
 	}
 	
 	private void assertCorrectRecanonicalization(
 			final String username,
 			final List<String> canonicalizedName,
 			final String version) {
-		final String flag = getFlag(version);
-		final Document user = getUserDoc(username);
-		@SuppressWarnings("unchecked")
-		final List<String> canon = (List<String>) user.get("dispcan");
-		assertThat("incorrect canonicalization", canon, is(canonicalizedName));
-		assertThat("missing flag", user.containsKey(flag), is(true));
-		if (user.containsKey(flag)) {
-			assertThat("incorrect flag value", user.getBoolean(flag), is(true));
-		}
+		MongoStorageTestCommon.assertCorrectRecanonicalization(
+				db, username, canonicalizedName, version);
 	}
 	
 	@Test
@@ -154,7 +96,7 @@ public class MongoStorageRecanonicalizationTest extends MongoStorageTester {
 			trashCanonicalizationData("user1");
 			method.invoke(
 					storage,
-					getFlag("7_6_3"),
+					getRecanonicalizationFlag("7_6_3"),
 					new Document("user", "user1").append("display", "bar foo"),
 					attempt
 			);
@@ -174,7 +116,7 @@ public class MongoStorageRecanonicalizationTest extends MongoStorageTester {
 			trashCanonicalizationData("user1");
 			method.invoke(
 					storage,
-					getFlag("0_0_1"),
+					getRecanonicalizationFlag("0_0_1"),
 					new Document("user", "user1").append("display", "bar foo"),
 					attempt
 			);
@@ -193,7 +135,7 @@ public class MongoStorageRecanonicalizationTest extends MongoStorageTester {
 		try {
 			method.invoke(
 					storage,
-					getFlag("7_6_3"),
+					getRecanonicalizationFlag("7_6_3"),
 					new Document("user", "user1").append("display", "bar foo"),
 					6
 			);
@@ -216,7 +158,7 @@ public class MongoStorageRecanonicalizationTest extends MongoStorageTester {
 		try {
 			method.invoke(
 					storage,
-					getFlag("0_0_1"),
+					getRecanonicalizationFlag("0_0_1"),
 					new Document("user", "user1").append("display", "bar foo"),
 					5
 			);
