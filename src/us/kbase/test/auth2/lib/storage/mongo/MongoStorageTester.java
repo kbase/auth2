3b0ee89f636838cc68527b605e96284023ace419
Sijie <sijiex@lbl.gov>
2024-01-09 17:07:15 -0500

    import new MongoController

--- src/us/kbase/test/auth2/lib/storage/mongo/MongoStorageTester.java
+++ src/us/kbase/test/auth2/lib/storage/mongo/MongoStorageTester.java
@@ -12,7 +12,7 @@ import com.mongodb.client.MongoDatabase;
 
 import us.kbase.auth2.cryptutils.RandomDataGenerator;
 import us.kbase.auth2.lib.storage.mongo.MongoStorage;
-import us.kbase.common.test.controllers.mongo.MongoController;
+import us.kbase.test.auth2.MongoController;
 import us.kbase.test.auth2.MongoStorageTestManager;
 
 public class MongoStorageTester {
