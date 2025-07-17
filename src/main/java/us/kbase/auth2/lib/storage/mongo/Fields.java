219d3281a750e48d150b9f51dd3a98d9341c7cd7
David Lyon <5115845+dauglyon@users.noreply.github.com>
2025-07-17 15:21:59 -0700

    update token field name

--- src/main/java/us/kbase/auth2/lib/storage/mongo/Fields.java
+++ src/main/java/us/kbase/auth2/lib/storage/mongo/Fields.java
@@ -138,7 +138,7 @@ public class Fields {
 	/** A value for a custom context key / value pair. */
 	public static final String TOKEN_CUSTOM_VALUE = "v";
 	/** Whether the token was created with multi-factor authentication. */
-	public static final String TOKEN_MFA_AUTHENTICATED = "mfaauth";
+	public static final String TOKEN_MFA_AUTHENTICATED = "mfa";
 	
 	/* ************************
 	 * temporary session data fields
